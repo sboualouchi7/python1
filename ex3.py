4b719acd5a213f2d8d3bef2856944a2a041bcf68
sboualouchi7 <salmanboualouchi@gmail.com>
2025-03-10 16:57:54 +0100

    ex3

---
+++ ex3.py
@@ -0,0 +1,8 @@
+#Exercice3
+name = "Salman"
+user_name = input("What is your name? ")
+
+if user_name ==name:
+    print("Hey, we have the same name!")
+else:
+    print(f"Nice to meet you, {user_name}! I'm {name}, but you already knew that, right?")
