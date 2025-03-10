ee50648888e4404d71b0fc85eb71a637b52e8b8d
sboualouchi7 <salmanboualouchi@gmail.com>
2025-03-10 16:59:31 +0100

    ex4

---
+++ ex4.py
@@ -0,0 +1,7 @@
+#Exercice4
+height = int(input("Enter your height in centimeters: "))
+
+if height > 145:
+    print("You are tall enough to ride!")
+else:
+    print("You need to grow some more to ride.")
