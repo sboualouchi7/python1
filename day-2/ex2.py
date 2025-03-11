ec8c04ef5f276e70322d127cefd55b75d15dfaff
sboualouchi7 <salmanboualouchi@gmail.com>
2025-03-11 15:54:37 +0000

    day-2 & ex2

---
+++ day-2/ex2.py
@@ -0,0 +1,16 @@
+
+family = {"rick": 43, 'beth': 13, 'morty': 5, 'summer': 8}
+total_cost = 0
+for name, age in family.items():
+    if age < 3:
+        ticket_price = 0
+    elif 3 <= age <= 12:
+        ticket_price = 10
+    else:
+        ticket_price = 15
+
+    print(f"{name} has to pay: ${ticket_price}")
+
+    total_cost += ticket_price
+
+print(f"Total cost for the family: ${total_cost}")
