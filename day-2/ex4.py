3e20880344598e23e9add3c659aa9b39dcaae019
sboualouchi7 <salmanboualouchi@gmail.com>
2025-03-12 10:00:32 +0000

    day-2 & ex4

---
+++ day-2/ex4.py
@@ -0,0 +1,6 @@
+def describe_city(city, country):
+    city = input("enter the city name")
+    country = input("enter the country name")
+    print("the "+city+" is in "+country)
+
+describe_city(city="Chicago", country="US")
